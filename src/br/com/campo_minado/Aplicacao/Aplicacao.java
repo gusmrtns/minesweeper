625ce37f701401573ac9223fad2127f6f08871c2
gusmrtns <gustavomartins9029@gmail.com>
2024-05-04 08:55:16 -0300

    criação da classe Tabuleiro e Aplicacao, Tabuleiro finalizada

---
+++ src/br/com/campo_minado/Aplicacao/Aplicacao.java
@@ -0,0 +1,15 @@
+package Aplicacao;
+
+import modelo.Tabuleiro;
+
+public class Aplicacao {
+
+	public static void main(String[] args) {
+		
+		Tabuleiro tabuleiro = new Tabuleiro(6, 6, 6);
+		
+		System.out.println(tabuleiro);
+		
+	}
+	
+}
